803453297f67b0135f48511df3275a174116e971
mk <mk@mks-MacBook-Air-2.local>
2022-10-07 15:36:19 -0400

    Updates:

---
+++ pages/people/mk/react-compare-image-demo.js
@@ -0,0 +1,13 @@
+import ReactCompareImage from 'react-compare-image';
+
+const ReactCompareDemo = () => {
+  return (
+    <div>
+      <h1>image comparison</h1>
+      <ReactCompareImage leftImage="/images/bb-sample.mp4-still-0005.png" rightImage="/images/bb-sample.mp4-still-0010.png" />;
+
+    </div>
+  )
+}
+
+export default ReactCompareDemo;
